bd39c4f9868a7cb1896b4b302324334b9caaf335
Anurag Gupta <aguptaking@gmail.com>
2020-08-08 02:07:11 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/javascript-playground/precache-manifest.d217868557a09875ace7809d5699192a.js"
+  "/javascript-playground/precache-manifest.82bd0d1e4acd22eeb74146e0c0c92bc2.js"
 );
 
 self.addEventListener('message', (event) => {
